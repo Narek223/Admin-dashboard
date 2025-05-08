ea548a434e9b5ce509ed803cc8ae39bae5adb18f
Narek223 <panosyan-narek@mail.ru>
2025-05-08 22:11:30 +0400

    inbox message

--- src/Components/Inbox/Inbox.jsx
+++ src/Components/Inbox/Inbox.jsx
@@ -6,6 +6,8 @@ import { FaComments } from "react-icons/fa";
 import { FaRegPaperPlane } from "react-icons/fa";
 import { GoBell } from "react-icons/go";
 import { AiOutlineMore } from "react-icons/ai";
+import { Menu, MenuItem } from "@mui/material";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 const Users = [
   {
@@ -32,13 +34,26 @@ export default function Inbox() {
   const [dialog, setdialog] = useState(false);
   const [dialogheader, setdialogheader] = useState(true);
   const [user, setuser] = useState();
-
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const usersMessages = (el) => {
     setdialog(true);
     setdialogheader(false);
 
     setuser(el);
+  };
+
+  const deletefunc = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const deletedialog = () => {
+    setmessages((prev) => prev.filter((item) => item.id !== user.id));
+
+    setuser(null);
+    setAnchorEl(null);
+    setdialog(false);
+    setdialogheader(true);
   };
   return (
     <div className={styles.inbox}>
@@ -57,7 +72,7 @@ export default function Inbox() {
             <input type="text" placeholder="Search" />
           </div>
 
-          {!messages ? (
+          {!messages  ||  messages.length === 0? (
             <div className={styles.noDialogs}>
               <FaRegPaperPlane className={styles.icon} />
               <p>No messages yet</p>
@@ -65,7 +80,7 @@ export default function Inbox() {
             </div>
           ) : (
             <div className={styles.userbox}>
-              {Users.map((elem, index) => (
+              {messages.map((elem, index) => (
                 <div
                   key={index}
                   className={styles.users}
@@ -90,13 +105,52 @@ export default function Inbox() {
             className={styles.Message_Header}
             style={{ display: dialog ? "block" : "none" }}
           >
-          <div className={styles.header}>
-            <div className={styles.headerbox}>
-              <img src={user?.avatar} alt="avatar" />
-              <h1>{user?.name}</h1>
+            <div className={styles.header}>
+              <div className={styles.headerbox}>
+                <img src={user?.avatar} alt="avatar" />
+                <h1>{user?.name}</h1>
+              </div>
+              <button
+                className={styles.infobtn}
+                aria-haspopup="true"
+                aria-expanded={false}
+                onClick={(event) => deletefunc(event)}
+              >
+                <AiOutlineMore className={styles.icon} />
+              </button>
+
+              <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={() => setAnchorEl(null)}
+                disablePortal
+                sx={{
+                  "& .MuiPaper-root": {
+                    width: "148px",
+                    backgroundColor: "rgba(248, 249, 250, 1)",
+                    borderRadius: "8px",
+                    padding: 0,
+                    boxShadow: "none",
+                  },
+                  "& .MuiMenuItem-root:hover": {
+                    backgroundColor: "white",
+                  },
+                }}
+              >
+                <MenuItem
+                  sx={{
+                    color: "red",
+                  }}
+                  onClick={deletedialog}
+                >
+                  <FaRegTrashAlt
+                    className={styles.newicon}
+                    style={{ marginRight: "12px" }}
+                  />
+                  Delete
+                </MenuItem>
+              </Menu>
             </div>
-            <AiOutlineMore className={styles.icon}/>
-          </div>
           </div>
           <div
             className={styles.messagesBox}
